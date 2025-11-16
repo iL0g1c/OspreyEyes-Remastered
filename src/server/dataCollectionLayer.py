c3386977db882bd8b8dcfd6a23c960cd09bd1c57
iL0g1c <83978815+iL0g1c@users.noreply.github.com>
2025-11-15 20:15:31 -0500

    Update dataCollectionLayer.py

--- src/server/dataCollectionLayer.py
+++ src/server/dataCollectionLayer.py
@@ -378,7 +378,6 @@ class DataCollectionLayer():
                     self.teleportationLogs.info(f"Account ID: {uid} teleported {round(dist)} km.")
                     evts.append({'eventType':'teleportation','oldLatitude':old[0],'oldLongitude':old[1],'newLatitude':pos[0],'newLongitude':pos[1],'timestamp':datetime.now(),'distance':dist})
 
-                    print({'url':'http://localhost:5002/teleporation','data':{'acid': uid, 'oldLatitude':old[0],'oldLongitude':old[1],'newLatitude':pos[0],'newLongitude':pos[1],'timestamp':datetime.now().strftime("%Y-%m-%d %H:%M:%S"),'distance':dist}})
                     self.queues['teleporation'].put({'url':'http://localhost:5002/teleporation','data':{'acid': uid, 'oldLatitude':old[0],'oldLongitude':old[1],'newLatitude':pos[0],'newLongitude':pos[1],'timestamp':datetime.now().strftime("%Y-%m-%d %H:%M:%S"),'distance':dist}})
 
             # aircraft change
