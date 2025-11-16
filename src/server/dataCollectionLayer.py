910c7760e0ccef71b1a146ff5187c4774b1ec034
iL0g1c <83978815+iL0g1c@users.noreply.github.com>
2025-11-15 19:49:13 -0500

    Update dataCollectionLayer.py

--- src/server/dataCollectionLayer.py
+++ src/server/dataCollectionLayer.py
@@ -377,7 +377,7 @@ class DataCollectionLayer():
                     self.teleportationLogs.info(f"Account ID: {uid} teleported {round(dist)} km.")
                     evts.append({'eventType':'teleportation','oldLatitude':old[0],'oldLongitude':old[1],'newLatitude':pos[0],'newLongitude':pos[1],'timestamp':datetime.now(),'distance':dist})
                     
-                    self.queues['teleporation'].put({'url':'http://localhost:5001/teleporation','data':{'acid': uid, 'oldLatitude':old[0],'oldLongitude':old[1],'newLatitude':pos[0],'newLongitude':pos[1],'timestamp':datetime.now(),'distance':dist}})
+                    self.queues['teleporation'].put({'url':'http://localhost:5002/teleporation','data':{'acid': uid, 'oldLatitude':old[0],'oldLongitude':old[1],'newLatitude':pos[0],'newLongitude':pos[1],'timestamp':datetime.now(),'distance':dist}})
             # aircraft change
             old_ac = exist_map.get(uid, {}).get('currentAircraft')
             if configs['logAircraftChanges'] and old_ac and ac != old_ac:
